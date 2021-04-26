029f8b622f7125cd9e388c88970c29e588be7ce9
JAYANTA BANIK <35198097+jayanta-banik@users.noreply.github.com>
2021-04-27 00:43:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 import os
+from pdf2image import convert_from_path
+import pytesseract
 
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+path_to_poopler = r"C:/Users/JOY/Desktop/poppler-21.02.0/Library/bin"
 app = Flask(__name__)
 # app.config['UPLOAD_FOLDER']
 # app.config['MAX_CONTENT_PATH']
@@ -18,13 +22,14 @@ def upload_files():
       f.save(secure_filename(f.filename))
 
       # check file extension 
-
+      imgs = convert_from_path(f.filename, poppler_path=path_to_poopler)
+      img = imgs[0]
       # img to ocr
-
+      s = pytesseract.image_to_string(img)
       # get text
 
       # return text
-      return redirect('https://www.google.com')
+      return s
 		
 if __name__ == '__main__':
    app.run(debug = True)
